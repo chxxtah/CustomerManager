6a5015e93d7868a26916737cb80471f0d7716c2d
chxxtah <apolegaroi02@gmail.com>
2026-06-29 18:43:54 +0800

    Create app/src/main/java/com/example/customermanager/data/Customer.kt

---
+++ app/src/main/java/com/example/customermanager/app/src/main/java/com/example/customermanager/data/Customer.kt
@@ -0,0 +1,11 @@
+package com.example.customermanager.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "customers")
+data class Customer(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val name: String,
+    val phone: String
+)
